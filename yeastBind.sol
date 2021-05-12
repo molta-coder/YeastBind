ee29f649c30340eb54d76faeca9a50ad47f68dd4
molta-coder <63711730+molta-coder@users.noreply.github.com>
2021-05-12 17:13:17 +0200

    cumulatively, maximum financial value added

--- yeastBind.sol
+++ yeastBind.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0;
 
- 
-
 contract YeastBind {
     address payable public beneficiary;
-    uint public size;
-    uint public biddingEnd;
-    uint public revealEnd;
+    uint public YeastSize;
+    uint remainingSize = YeastSize;
+    uint biddingEnd;
+    uint revealEnd;
     bool public ended;    
     
       struct Bid{
@@ -15,21 +14,22 @@ contract YeastBind {
         uint deposit;
     }
     
-    mapping(address => Bid[]) public bids;
+    mapping(address => Bid[])  bids;
     // Allowed withdrawals of previous bids
-    mapping(address => uint) pendingReturns;
+    //mapping(address => uint) pendingReturns;
     
-    address public highestBidder;
-    uint public highestBid;
+    address[] highestBidders;
+    uint[]  highestBids;
+    uint[]  highestQuantitys;
     
-    event AuctionEnded(address winner, uint highestBid);
+    event AuctionEnded(address winner, uint highestBid, uint highestQuantity);
     
     modifier onlyBefore(uint _time)  { require(block.timestamp < _time); _; }
     modifier onlyAfter(uint _time) { require(block.timestamp > _time); _; }
     
-    constructor(uint _biddingTime, uint _revealTime, address payable _beneficiary, uint _size){
+    constructor(uint _biddingTime, uint _revealTime, address payable _beneficiary, uint _YeastSize){
         beneficiary = _beneficiary;
-        size = _size;
+        YeastSize = _YeastSize;
         biddingEnd = block.timestamp +_biddingTime;
         revealEnd = biddingEnd + _revealTime;
     }
@@ -56,37 +56,53 @@ contract YeastBind {
             refund += bidToCheck.deposit;
             if ( bidToCheck.deposit >= price) {
                 if (placeBid(msg.sender, price, quantity))
-                    refund -= value;
+                    refund -= price;
             }
             bidToCheck.hasOfBid = bytes32(0);
         }
         payable(msg.sender).transfer(refund);
     }
     
-    function withdraw() public{
+    /*function withdraw() public{
         uint amount = pendingReturns[msg.sender];
         if (amount > 0){
             pendingReturns[msg.sender] = 0;
             payable(msg.sender).transfer(amount);
         }
-    }
+    }*/
     
     function auctionEnd() public onlyAfter(revealEnd){
         require (!ended);
-        emit AuctionEnded(highestBidder, highestBid);
+        uint length = highestBidders.length;
+        uint prize = 0;
+        for (uint i = length; i > 0; i++){
+            if (remainingSize >= highestQuantitys[i]){
+                remainingSize -= highestQuantitys[i];
+                prize += highestBids[i];
+                emit AuctionEnded(highestBidders[i], highestBids[i], highestQuantitys[i]);
+            }
+        }
         ended = true;
-        beneficiary.transfer(highestBid);
+        beneficiary.transfer(prize);
     }
     
     function placeBid(address bidder, uint price, uint quantity) internal returns (bool success){
-        if (value <= highestBid){
-            return false;
+        uint length = highestBidders.length;
+        if (length == 0 && quantity <= YeastSize){
+                highestBids.push(price);
+                highestBidders.push(bidder);
+                highestQuantitys.push(quantity);
+                return true;
+            }
+            
+        for (uint i = 0; i < length; i++){
+            if (price > highestBids[i] && quantity <= YeastSize){
+                highestBids.push(price);
+                highestBidders.push(bidder);
+                highestQuantitys.push(quantity);
+                return true;
+            }
         }
-        if (highestBidder != address(0)){
-            pendingReturns[highestBidder] += highestBid;
-        }
-        highestBid = value;
-        highestBidder = bidder;
-        return true;
+        return false;
     }
 }
